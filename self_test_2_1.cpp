8d027c7f25110c1b5c2f2c52d70efd4308301fbe
YuexianChen <100741890+YuexianChen@users.noreply.github.com>
2022-03-28 22:19:27 +1030

    Add files via upload

---
+++ self_test_2_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Q1
+    int feet=0, inches=0;
+
+    //Q2
+    int count = 0;
+    double distance = 1.5;
+
+    //Q3
+    int sum=0, n1, n2;
+    sum = n1+n2;
+
+    //Q4
+    double length=0;
+    cin >> length;
+    length = length + 8.3;
+
+    //Q5
+    int n,old_value, target_value;
+    cout << "enter a value: \n" ;
+    cin >> old_value;
+    cout << "enter how many times you want to produce: ";
+    cin >> n;
+    target_value = old_value * n;
+    return 0;
+}
