56342f2350d3ae9138ba8af38e922d137c2bd79e
YuexianChen <100741890+YuexianChen@users.noreply.github.com>
2022-04-01 21:58:05 +1030

    Add files via upload

---
+++ display-2-12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int countDown;
+    cout << "How many greetings do you want? ";
+    cin >> countDown;
+
+    while(countDown > 0)
+    {
+        cout << "Hello ";
+        countDown--;
+    }
+    cout << endl;
+    cout << "That's all!\n";
+    return 0;
+}
