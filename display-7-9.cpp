9d11f82043005bbefe893ff9e9e9d954e7679a9d
YuexianChen <100741890+YuexianChen@users.noreply.github.com>
2022-04-08 16:10:49 +0930

    Add files via upload

---
+++ display-7-9.cpp
@@ -0,0 +1,73 @@
+//Shows the difference between each of a list of golf scores and their average
+#include <iostream>
+const int MAX_NUMBER_SCORES = 10;
+
+void fillArray(int a[], int size, int &numberUsed);
+double computeAverage(const int a[], int numberUsed);
+void showDifference(const int a[], int numberUsed);
+
+int main()
+{
+    using namespace std;
+    int score[MAX_NUMBER_SCORES], numberUsed;
+
+    cout << "This program reads golf scores and shows\n"
+        << "how much each differs from the average.\n";
+    
+    cout << "Enter golf scores:\n";
+    fillArray(score, MAX_NUMBER_SCORES, numberUsed);
+    showDifference(score, numberUsed);
+    return 0;
+}
+
+//Uses iostream:
+void fillArray(int a[], int size, int &numberUsed)
+{
+    using namespace std;
+    cout << "Enter uo to " << size << " nonegative whole numbers.\n"
+        << "Mark the end of the list with a negative number.\n";
+    int next, index=0;
+    cin >> next;
+    while((next >= 0) &&(index <size))
+    {
+        a[index] = next;
+        index++;
+        cin >> next;
+    }
+    numberUsed = index;
+}
+
+double computeAverage(const int a[], int numberUsed)
+{
+    double total =0;
+    for(int index=0; index < numberUsed; index++)
+    {
+        total = total + a[index];
+    }
+
+    if (numberUsed > 0)
+    {
+        return (total/numberUsed);
+    }
+    else
+    {
+        using namespace std;
+        cout << "ERROR: number of elements is 0 in computeAverage.\n"
+            << "computeAverage returns 0.\n";
+        return 0;
+    }
+}
+void showDifference(const int a[], int numberUsed)
+{
+    using namespace std;
+    double average = computeAverage(a, numberUsed);
+    cout << "Average of the " << numberUsed 
+        << " scores = " << average << endl
+        << "The scores are:\n";
+    for(int index = 0; index < numberUsed; index++)
+    {
+        cout << a[index] << " differes from average by "
+            << (a[index]-average) << endl;
+    }
+}
+
